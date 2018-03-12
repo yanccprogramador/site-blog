27ea3ae9068986aee08a7b02dc809893fc01a305
yan <yan@yan-linux.yanchristoffer>
2018-03-12 11:55:22 -0300

    improving seo and accessiility

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,7 @@
     <div class="col s12 m3">
       <div class="card">
         <div class="card-image">
-          <img :src="img" />
+          <img :src="img" alt="Yan"/>
           <span class="card-title">
                                     {{name}} @{{login}}</span>
         </div>
@@ -31,36 +31,36 @@
       Trabalhamos com:
       <br/>
       <ul class="col m4">
-        <li><img src="../assets/ai.png" class="circle" width="40px" height="40px"><a href="http://www.adobe.com/br/products/illustrator.html">Adobe Illustrator</a></li>
-        <li><img src="../assets/ang.png" class="circle" width="40px" height="40px"><a href="https://angular.io/">Angular 1.x e 2+</a></li>
-        <li><img src="../assets/autocad.png" class="circle" width="40px" height="40px"><a href="http://www.autodesk.com/education/free-software/autocad">Autocad</a></li>
-        <li><img src="../assets/bootstrap.png" class="circle" width="40px" height="40px"><a href="https://v4-alpha.getbootstrap.com/">Bootstrap</a></li>
-        <li><img src="../assets/c.png" class="circle" width="40px" height="40px"><a href="https://msdn.microsoft.com/pt-BR/library/kx37x362.aspx">C#</a></li>
-        <li><img src="../assets/cpp.png" class="circle" width="40px" height="40px"><a href="https://pt.wikipedia.org/wiki/C%2B%2B">C++</a></li>
-        <li><img src="../assets/django.png" class="circle" width="40px" height="40px"><a href="https://www.djangoproject.com/">Django</a></li>
-        <li><img src="../assets/java.png" class="circle" width="40px" height="40px"><a href="https://www.java.com/pt_BR/">Java</a></li>
-        <li><img src="../assets/js.png" class="circle" width="40px" height="40px"><a href="https://www.javascript.com/">JavaScript</a></li>
-        <li><img src="../assets/laravel.png" class="circle" width="40px" height="40px"><a href="https://laravel.com/">Laravel</a></li>
+        <li><img src="../assets/ai.png" class="circle" width="40px" height="40px" alt="Illustrator"><a href="http://www.adobe.com/br/products/illustrator.html">Adobe Illustrator</a></li>
+        <li><img src="../assets/ang.png" class="circle" width="40px" height="40px" alt="Angular"><a href="https://angular.io/">Angular 1.x e 2+</a></li>
+        <li><img src="../assets/autocad.png" class="circle" width="40px" height="40px" alt="Autocad"><a href="http://www.autodesk.com/education/free-software/autocad">Autocad</a></li>
+        <li><img src="../assets/bootstrap.png" class="circle" width="40px" height="40px" alt="Bootstrap"><a href="https://v4-alpha.getbootstrap.com/">Bootstrap</a></li>
+        <li><img src="../assets/c.png" class="circle" width="40px" height="40px" alt="C#"><a href="https://msdn.microsoft.com/pt-BR/library/kx37x362.aspx">C#</a></li>
+        <li><img src="../assets/cpp.png" class="circle" width="40px" height="40px" alt="C++"><a href="https://pt.wikipedia.org/wiki/C%2B%2B">C++</a></li>
+        <li><img src="../assets/django.png" class="circle" width="40px" height="40px" alt="Django"><a href="https://www.djangoproject.com/">Django</a></li>
+        <li><img src="../assets/java.png" class="circle" width="40px" height="40px" alt="Java"><a href="https://www.java.com/pt_BR/">Java</a></li>
+        <li><img src="../assets/js.png" class="circle" width="40px" height="40px" alt="JavaScript"><a href="https://www.javascript.com/">JavaScript</a></li>
+        <li><img src="../assets/laravel.png" class="circle" width="40px" height="40px" alt="Laravel"><a href="https://laravel.com/">Laravel</a></li>
       </ul>
       <ul class="col m4">
-        <li><img src="../assets/materialize.png" class="circle" width="40px" height="40px"><a href="http://materializecss.com/">Materialize</a></li>
-        <li><img src="../assets/sql-server.png" class="circle" width="40px" height="40px"><a href="https://www.microsoft.com/pt-br/sql-server/sql-server-2017">Microsoft Sql Server</a></li>
-        <li><img src="../assets/mongo.png" class="circle" width="40px" height="40px"><a href="https://www.mongodb.com/?_ga=2.178384740.1545352998.1510785540-1928681075.1508100057">Mongo DB</a></li>
-        <li><img src="../assets/Mysql.png" class="circle" width="40px" height="40px"><a href="https://www.mysql.com/">Mysql</a></li>
-        <li><img src="../assets/nodejs.png" class="circle" width="40px" height="40px"><a href="http://nodejs.org/">Node.Js</a></li>
-        <li><img src="../assets/nwjs.png" class="circle" width="40px" height="40px"><a href="https://nwjs.io/">NW.js(Node Webkit)</a></li>
-        <li><img src="../assets/pf.png" class="circle" width="40px" height="40px"><a href="https://www.primefaces.org/">Primefaces</a></li>
-        <li><img src="../assets/php.png" class="circle" width="40px" height="40px"><a href="https://secure.php.net/manual/pt_BR/">PHP</a></li>
-        <li><img src="../assets/postgresql.png" class="circle" width="40px" height="40px"><a href="https://www.postgresql.org/">PostgreSql</a></li>
-        <li><img src="../assets/ps.png" class="circle" width="40px" height="40px"><a href="http://www.adobe.com/br/products/photoshop.html">Photoshop</a></li>
+        <li><img src="../assets/materialize.png" class="circle" width="40px" height="40px" alt="Materialize"><a href="http://materializecss.com/">Materialize</a></li>
+        <li><img src="../assets/sql-server.png" class="circle" width="40px" height="40px" alt="Sql Server"><a href="https://www.microsoft.com/pt-br/sql-server/sql-server-2017">Microsoft Sql Server</a></li>
+        <li><img src="../assets/mongo.png" class="circle" width="40px" height="40px" alt="Mongodb"><a href="https://www.mongodb.com/?_ga=2.178384740.1545352998.1510785540-1928681075.1508100057">Mongo DB</a></li>
+        <li><img src="../assets/Mysql.png" class="circle" width="40px" height="40px" alt="Mysql"><a href="https://www.mysql.com/">Mysql</a></li>
+        <li><img src="../assets/nodejs.png" class="circle" width="40px" height="40px" alt="Node.js"><a href="http://nodejs.org/">Node.Js</a></li>
+        <li><img src="../assets/nwjs.png" class="circle" width="40px" height="40px" alt="Nw.js"><a href="https://nwjs.io/">NW.js(Node Webkit)</a></li>
+        <li><img src="../assets/pf.png" class="circle" width="40px" height="40px" alt="Primefaces"><a href="https://www.primefaces.org/">Primefaces</a></li>
+        <li><img src="../assets/php.png" class="circle" width="40px" height="40px" alt="PHP"><a href="https://secure.php.net/manual/pt_BR/">PHP</a></li>
+        <li><img src="../assets/postgresql.png" class="circle" width="40px" height="40px" alt="Postgres Sql"><a href="https://www.postgresql.org/">PostgreSql</a></li>
+        <li><img src="../assets/ps.png" class="circle" width="40px" height="40px" alt="Photoshop"><a href="http://www.adobe.com/br/products/photoshop.html">Photoshop</a></li>
       </ul>
       <ul class="col m4">
-        <li><img src="../assets/python.png" class="circle" width="40px" height="40px"><a href="https://python.org/">Python</a></li>
-        <li><img src="../assets/react.png" class="circle" width="40px" height="40px"><a href="https://reactjs.org">React</a></li>
-        <li><img src="../assets/symfony.png" class="circle" width="40px" height="40px"><a href="https://symfony.com">Symfony</a></li>
-        <li><img src="../assets/unity.png" class="circle" width="40px" height="40px"><a href="https://www.postgresql.org/">Unity</a></li>
-        <li><img src="../assets/vue.png" class="circle" width="40px" height="40px"><a href="https://www.vuejs.com/">VueJs</a></li>
-        <li><img src="../assets/zend.png" class="circle" width="40px" height="40px"><a href="https://docs.zendframework.com/zend-expressive/">Zend Expressive</a></li>
+        <li><img src="../assets/python.png" class="circle" width="40px" height="40px" alt="Python"><a href="https://python.org/">Python</a></li>
+        <li><img src="../assets/react.png" class="circle" width="40px" height="40px" alt="React"><a href="https://reactjs.org">React</a></li>
+        <li><img src="../assets/symfony.png" class="circle" width="40px" height="40px" alt="Symfony"><a href="https://symfony.com">Symfony</a></li>
+        <li><img src="../assets/unity.png" class="circle" width="40px" height="40px" alt="Unity"><a href="https://www.postgresql.org/">Unity</a></li>
+        <li><img src="../assets/vue.png" class="circle" width="40px" height="40px" alt="VueJS"><a href="https://www.vuejs.com/">VueJs</a></li>
+        <li><img src="../assets/zend.png" class="circle" width="40px" height="40px" alt="Zend Expressive"><a href="https://docs.zendframework.com/zend-expressive/">Zend Expressive</a></li>
 
       </ul>
     </div>
