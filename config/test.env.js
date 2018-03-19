edd0ef492757a5a23f1d50f882d2a3ba686216c8
yan <yan@yan-linux.yanchristoffer>
2018-03-18 21:08:23 -0300

    testing

--- config/test.env.js
+++ config/test.env.js
@@ -4,5 +4,6 @@ const merge = require('webpack-merge')
 const devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
-  NODE_ENV: '"testing"'
+  NODE_ENV: '"testing"',
+  ACCESS_TOKEN: process.env.ACCESS_TOKEN
 })
